d6958fefcf504980384da4852f1d1f9db9ba14e2
sarmozo <yoannndori@gmail.com>
2025-06-18 11:08:58 +0000

    Exercise completed, used the round() function as an extra to reduce the number after the comma.

--- radian_converter.py
+++ radian_converter.py
@@ -1,5 +1,7 @@
-radian = float(input("Enter the number of radian : "))
+radians = float(input("Enter the number of radian : "))
 
-degree = radian *  180/3.14
+degrees = radians *  180/3.14
+minutes = degrees * 60
+seconds = minutes * 60
 
-print(radian , "radian in degree is equal to :" , degree)
+print(radians , "radians is equal to:" , round(degrees , 3) , "degrees ," , round(minutes , 3) , "minutes ," , round(seconds, 3) , "seconds")
