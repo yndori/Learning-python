de77e232834bb5d130bce4652e547bc723ec0c06
sarmozo <yoannndori@gmail.com>
2025-06-28 00:23:26 +0000

    First commit

---
+++ print_clear_list.py
@@ -0,0 +1,9 @@
+t2 = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+c = 0
+i = 0
+
+while c < len(t2):
+    print(t2[i] , end=" ")
+    i += 1
+    c += 1
