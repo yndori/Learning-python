e2fa1bb14650a2fc5a59a3b57958b6e8e405b13e
sarmozo <yoannndori@gmail.com>
2025-06-28 15:18:10 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,5 @@
+miles_per_hour = int(input("Enter the distance in miles/hour : "))
+
+kilometers_per_hour = miles_per_hour * 1.609
+
+print(f"{miles_per_hour} MPH is equal to {kilometers_per_hour} KPH")
