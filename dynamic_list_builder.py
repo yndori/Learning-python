1ae02104226fd4e09cafc3ec7a781dd48f9dc763
sarmozo <yoannndori@gmail.com>
2025-07-02 17:23:21 +0000

    changed the condition

--- dynamic_list_builder.py
+++ dynamic_list_builder.py
@@ -1,7 +1,7 @@
 a = []
 b = input("Enter a new value to be added to the list. If you're done write 'enter' : ")
 
-while b.lower() != "enter" :
+while b != "" :
     a.append(b)
     b = input("Enter a new value to be added to the list. If you're done write 'enter' : ")
 
