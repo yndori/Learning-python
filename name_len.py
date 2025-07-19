79a1fc1f0fb63daf7b349216c46161e9147b093a
sarmozo <yoannndori@gmail.com>
2025-07-19 15:20:57 +0000

    First commit

---
+++ name_len.py
@@ -0,0 +1,8 @@
+names = ['Jean-Michel', 'Marc', 'Vanessa', 'Anne', 'Maximilien',
+'Alexandre-Benoît', 'Louise']
+
+i = 0
+
+while i < len(names):
+    print(names[i] ,"is",len(names[i]),"characters.")
+    i += 1
