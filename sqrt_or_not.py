3ec7b1b78459e3c47d2475460c3c66954bd52521
sarmozo <yoannndori@gmail.com>
2025-07-19 14:23:26 +0000

    Update

--- sqrt_or_not.py
+++ sqrt_or_not.py
@@ -5,7 +5,7 @@ a = int(input("Enter an integer number: "))
 if a < 0:
     print(f"The number {a} is negative, so its square root cannot be calculated in real numbers.")
 else:
-    integer_root = math.isqrt(a)
+    integer_root = isqrt(a)
     if integer_root * integer_root == a:
         print(f"The exact square root of {a} is {integer_root}.")
     else:
