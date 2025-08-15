72bd720532b9dd92698c198ccb0084da02f3448b
sarmozo <yoannndori@gmail.com>
2025-08-15 23:39:21 +0000

    Update

--- surfCircle.py
+++ surfCircle.py
@@ -5,6 +5,6 @@ def surfCircle(R):
     area = math.pi * R**2
     return area
 
-print(surfCircle(__doc__))
+print(surfCircle.__doc__)
 R = int(input("Enter the radius R : "))
 print(f"The radius of the circle is : {surfCircle(R):.2f}")
