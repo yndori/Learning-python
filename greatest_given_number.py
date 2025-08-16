9fbf5d2d03f95a9bbe876234668a673924e1b2df
sarmozo <yoannndori@gmail.com>
2025-08-16 23:40:15 +0000

    Update

--- greatest_given_number.py
+++ greatest_given_number.py
@@ -1,8 +1,9 @@
 def maximum(n1,n2,n3):
+    """Thhis function returns the greatest number between n1,n2,n3"""
     result = max(n1,n2,n3)
     return result
 
-n1 = int(input("Enter x1 : "))
-n2 = int(input("Enter x2 : "))
-n3 = int(input("Enter x3 : "))
+n1 = int(input("Enter n1 : "))
+n2 = int(input("Enter n2 : "))
+n3 = int(input("Enter n3 : "))
 print(f"The largest number is {maximum(n1,n2,n3)}")
