faf48e184f920bfda46ce642656e3a76521947b7
sarmozo <yoannndori@gmail.com>
2025-08-16 20:04:47 +0000

    First commit

---
+++ greatest_given_number.py
@@ -0,0 +1,8 @@
+def maximum(n1,n2,n3):
+    result = max(n1,n2,n3)
+    return result
+
+n1 = int(input("Enter x1 : "))
+n2 = int(input("Enter x2 : "))
+n3 = int(input("Enter x3 : "))
+print(f"The largest number is {maximum(n1,n2,n3)}")
